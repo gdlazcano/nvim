7848ecb09ed8f1cfead126c9d8ac90505049b649
Gabriel <gabriel.david.lazcano@gmail.com>
2024-12-27 12:30:55 -0600

    added copilot

---
+++ lua/gabriel/plugins/copilot.lua
@@ -0,0 +1,18 @@
+return {
+	"zbirenbaum/copilot.lua",
+	cmd = { "Copilot" },
+	event = { "InsertEnter" },
+	lazy = false,
+
+	config = function()
+		require("copilot").setup({
+			suggestion = {
+				auto_trigger = true,
+				debounce = 50,
+				keymap = {
+					accept = ";;",
+				},
+			},
+		})
+	end,
+}
